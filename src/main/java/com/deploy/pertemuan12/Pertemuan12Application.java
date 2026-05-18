1cd6518b7ed867efaf738995533eeb2ed25c8c61
salmaarayyan <salmaarayyan@gmail.com>
2026-05-18 10:35:43 +0700

    Membuat folder project

---
+++ src/main/java/com/deploy/pertemuan12/Pertemuan12Application.java
@@ -0,0 +1,13 @@
+package com.deploy.pertemuan12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pertemuan12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pertemuan12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/pertemuan12/Pertemuan12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.pertemuan12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
